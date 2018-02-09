6172c836e5bbe8963281338037873720f733fee8
Behroz Sikander <behroz89@gmail.com>
2018-02-09 23:03:32 +0100

    adding mysql files

---
+++ mysql/2_create_table2.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `Table2` (
+  `Id` int(11) NOT NULL,
+  `Col1` varchar(45) DEFAULT NULL,
+  `Col2` varchar(45) DEFAULT NULL,
+  `Col3` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
